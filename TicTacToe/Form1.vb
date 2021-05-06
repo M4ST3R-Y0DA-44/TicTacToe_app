0f6fac7b9f129d0139272cd161d92dd7acd6f3a7
Yannick <munyan4@gmail.com>
2021-05-06 00:16:47 -0400

    initial save

--- TicTacToe/Form1.vb
+++ TicTacToe/Form1.vb
@@ -2,6 +2,8 @@
 
     Private intPlayer1Wins As Integer
     Private intPlayer2Wins As Integer
+    Private intPlayerTurn As Integer
+
 
     Private Sub Reset_Grid()
         lblGrid1.Text = String.Empty
@@ -16,6 +18,7 @@
     End Sub
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        intPlayerTurn = 1
         intPlayer1Wins = 0
         intPlayer2Wins = 0
         lblPlayer1GamesWon.Text = intPlayer1Wins.ToString
@@ -33,8 +36,8 @@
             lblGrid8.Text + lblGrid9.Text
         If strContentOfGrid <> String.Empty Then
             Dim dlgAnswer As DialogResult
-            dlgAnswer = MessageBox.Show("New Game", "Starting a new game will reset both players wins total back to zero." + ControlChars.NewLine +
-                                        "Are you sure you want to start a new game?", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation)
+            dlgAnswer = MessageBox.Show("Starting a new game will reset both players wins total back to zero." + ControlChars.NewLine +
+                                        "Are you sure you want to start a new game?", "New Game", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation)
             If dlgAnswer = DialogResult.Yes Then
                 Reset_Grid()
                 intPlayer1Wins = 0
@@ -42,6 +45,75 @@
                 lblPlayer1GamesWon.Text = intPlayer1Wins.ToString
                 lblPlayer2Gameswon.Text = intPlayer2Wins.ToString
             End If
+        End If
+    End Sub
+
+
+
+    Private Function Player_Change() As String
+        If intPlayerTurn = 1 Then
+            intPlayerTurn = 2
+            Return "X"
+        Else
+            intPlayerTurn = 1
+            Return "O"
+        End If
+    End Function
+
+
+
+    Private Sub lblGrid1_Click(sender As Object, e As EventArgs) Handles lblGrid1.Click
+        If lblGrid1.Text = String.Empty Then
+            lblGrid1.Text = Player_Change()
+        End If
+    End Sub
+
+    Private Sub lblGrid2_Click(sender As Object, e As EventArgs) Handles lblGrid2.Click
+        If lblGrid2.Text = String.Empty Then
+            lblGrid2.Text = Player_Change()
+        End If
+    End Sub
+
+    Private Sub lblGrid3_Click(sender As Object, e As EventArgs) Handles lblGrid3.Click
+        If lblGrid3.Text = String.Empty Then
+            lblGrid3.Text = Player_Change()
+        End If
+    End Sub
+
+    Private Sub lblGrid4_Click(sender As Object, e As EventArgs) Handles lblGrid4.Click
+        If lblGrid4.Text = String.Empty Then
+            lblGrid4.Text = Player_Change()
+        End If
+    End Sub
+
+    Private Sub lblGrid5_Click(sender As Object, e As EventArgs) Handles lblGrid5.Click
+        If lblGrid5.Text = String.Empty Then
+            lblGrid5.Text = Player_Change()
+        End If
+    End Sub
+
+    Private Sub lblGrid6_Click(sender As Object, e As EventArgs) Handles lblGrid6.Click
+        If lblGrid6.Text = String.Empty Then
+            lblGrid6.Text = Player_Change()
+        End If
+    End Sub
+
+    Private Sub lblGrid7_Click(sender As Object, e As EventArgs) Handles lblGrid7.Click
+        If lblGrid7.Text = String.Empty Then
+            lblGrid7.Text = Player_Change()
+        End If
+
+    End Sub
+
+    Private Sub lblGrid8_Click(sender As Object, e As EventArgs) Handles lblGrid8.Click
+        If lblGrid8.Text = String.Empty Then
+            lblGrid8.Text = Player_Change()
+        End If
+    End Sub
+
+    Private Sub lblGrid9_Click(sender As Object, e As EventArgs) Handles lblGrid9.Click
+        If lblGrid9.Text = String.Empty Then
+            lblGrid9.Text = Player_Change()
         End If
     End Sub
 End Class
