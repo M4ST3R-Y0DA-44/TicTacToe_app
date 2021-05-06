051a90769a763e56641b09a30131392402af8dd9
Yannick <munyan4@gmail.com>
2021-05-05 23:52:55 -0400

    initial_save

--- TicTacToe/Form1.Designer.vb
+++ TicTacToe/Form1.Designer.vb
@@ -22,10 +22,232 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.lblGrid1 = New System.Windows.Forms.Label()
+        Me.lblGrid2 = New System.Windows.Forms.Label()
+        Me.lblGrid3 = New System.Windows.Forms.Label()
+        Me.lblGrid4 = New System.Windows.Forms.Label()
+        Me.lblGrid5 = New System.Windows.Forms.Label()
+        Me.lblGrid6 = New System.Windows.Forms.Label()
+        Me.lblGrid7 = New System.Windows.Forms.Label()
+        Me.lblGrid8 = New System.Windows.Forms.Label()
+        Me.lblGrid9 = New System.Windows.Forms.Label()
+        Me.lblPlayer1GamesWon = New System.Windows.Forms.Label()
+        Me.lblPlayer2Gameswon = New System.Windows.Forms.Label()
+        Me.btnQuit = New System.Windows.Forms.Button()
+        Me.btnNewGame = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(266, 30)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(339, 77)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "TicTacToe"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 153)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(57, 17)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Player 1:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 186)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(57, 17)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Player 2:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(79, 123)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(78, 17)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Games Won"
+        '
+        'lblGrid1
+        '
+        Me.lblGrid1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblGrid1.Font = New System.Drawing.Font("Segoe UI", 72.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblGrid1.Location = New System.Drawing.Point(211, 176)
+        Me.lblGrid1.Name = "lblGrid1"
+        Me.lblGrid1.Size = New System.Drawing.Size(148, 128)
+        Me.lblGrid1.TabIndex = 4
+        Me.lblGrid1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblGrid2
+        '
+        Me.lblGrid2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblGrid2.Font = New System.Drawing.Font("Segoe UI", 72.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblGrid2.Location = New System.Drawing.Point(356, 176)
+        Me.lblGrid2.Name = "lblGrid2"
+        Me.lblGrid2.Size = New System.Drawing.Size(148, 128)
+        Me.lblGrid2.TabIndex = 5
+        Me.lblGrid2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblGrid3
+        '
+        Me.lblGrid3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblGrid3.Font = New System.Drawing.Font("Segoe UI", 72.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblGrid3.Location = New System.Drawing.Point(501, 176)
+        Me.lblGrid3.Name = "lblGrid3"
+        Me.lblGrid3.Size = New System.Drawing.Size(148, 128)
+        Me.lblGrid3.TabIndex = 6
+        Me.lblGrid3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblGrid4
+        '
+        Me.lblGrid4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblGrid4.Font = New System.Drawing.Font("Segoe UI", 72.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblGrid4.Location = New System.Drawing.Point(211, 304)
+        Me.lblGrid4.Name = "lblGrid4"
+        Me.lblGrid4.Size = New System.Drawing.Size(148, 128)
+        Me.lblGrid4.TabIndex = 7
+        Me.lblGrid4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblGrid5
+        '
+        Me.lblGrid5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblGrid5.Font = New System.Drawing.Font("Segoe UI", 72.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblGrid5.Location = New System.Drawing.Point(356, 304)
+        Me.lblGrid5.Name = "lblGrid5"
+        Me.lblGrid5.Size = New System.Drawing.Size(148, 128)
+        Me.lblGrid5.TabIndex = 8
+        Me.lblGrid5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblGrid6
+        '
+        Me.lblGrid6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblGrid6.Font = New System.Drawing.Font("Segoe UI", 72.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblGrid6.Location = New System.Drawing.Point(501, 304)
+        Me.lblGrid6.Name = "lblGrid6"
+        Me.lblGrid6.Size = New System.Drawing.Size(148, 128)
+        Me.lblGrid6.TabIndex = 9
+        Me.lblGrid6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblGrid7
+        '
+        Me.lblGrid7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblGrid7.Font = New System.Drawing.Font("Segoe UI", 72.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblGrid7.Location = New System.Drawing.Point(211, 432)
+        Me.lblGrid7.Name = "lblGrid7"
+        Me.lblGrid7.Size = New System.Drawing.Size(148, 128)
+        Me.lblGrid7.TabIndex = 10
+        Me.lblGrid7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblGrid8
+        '
+        Me.lblGrid8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblGrid8.Font = New System.Drawing.Font("Segoe UI", 72.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblGrid8.Location = New System.Drawing.Point(356, 432)
+        Me.lblGrid8.Name = "lblGrid8"
+        Me.lblGrid8.Size = New System.Drawing.Size(148, 128)
+        Me.lblGrid8.TabIndex = 11
+        Me.lblGrid8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblGrid9
+        '
+        Me.lblGrid9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblGrid9.Font = New System.Drawing.Font("Segoe UI", 72.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblGrid9.Location = New System.Drawing.Point(501, 432)
+        Me.lblGrid9.Name = "lblGrid9"
+        Me.lblGrid9.Size = New System.Drawing.Size(148, 128)
+        Me.lblGrid9.TabIndex = 12
+        Me.lblGrid9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblPlayer1GamesWon
+        '
+        Me.lblPlayer1GamesWon.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblPlayer1GamesWon.Location = New System.Drawing.Point(79, 152)
+        Me.lblPlayer1GamesWon.Name = "lblPlayer1GamesWon"
+        Me.lblPlayer1GamesWon.Size = New System.Drawing.Size(78, 25)
+        Me.lblPlayer1GamesWon.TabIndex = 13
+        '
+        'lblPlayer2Gameswon
+        '
+        Me.lblPlayer2Gameswon.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblPlayer2Gameswon.Location = New System.Drawing.Point(79, 185)
+        Me.lblPlayer2Gameswon.Name = "lblPlayer2Gameswon"
+        Me.lblPlayer2Gameswon.Size = New System.Drawing.Size(78, 25)
+        Me.lblPlayer2Gameswon.TabIndex = 14
+        '
+        'btnQuit
+        '
+        Me.btnQuit.Location = New System.Drawing.Point(833, 536)
+        Me.btnQuit.Name = "btnQuit"
+        Me.btnQuit.Size = New System.Drawing.Size(88, 24)
+        Me.btnQuit.TabIndex = 15
+        Me.btnQuit.Text = "&Quit"
+        Me.btnQuit.UseVisualStyleBackColor = True
+        '
+        'btnNewGame
+        '
+        Me.btnNewGame.Location = New System.Drawing.Point(698, 471)
+        Me.btnNewGame.Name = "btnNewGame"
+        Me.btnNewGame.Size = New System.Drawing.Size(88, 24)
+        Me.btnNewGame.TabIndex = 16
+        Me.btnNewGame.Text = "&New Game"
+        Me.btnNewGame.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 17.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(933, 588)
+        Me.Controls.Add(Me.btnNewGame)
+        Me.Controls.Add(Me.btnQuit)
+        Me.Controls.Add(Me.lblPlayer2Gameswon)
+        Me.Controls.Add(Me.lblPlayer1GamesWon)
+        Me.Controls.Add(Me.lblGrid9)
+        Me.Controls.Add(Me.lblGrid8)
+        Me.Controls.Add(Me.lblGrid7)
+        Me.Controls.Add(Me.lblGrid6)
+        Me.Controls.Add(Me.lblGrid5)
+        Me.Controls.Add(Me.lblGrid4)
+        Me.Controls.Add(Me.lblGrid3)
+        Me.Controls.Add(Me.lblGrid2)
+        Me.Controls.Add(Me.lblGrid1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Name = "Form1"
+        Me.Text = "TicTacToe"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents lblGrid1 As Label
+    Friend WithEvents lblGrid2 As Label
+    Friend WithEvents lblGrid3 As Label
+    Friend WithEvents lblGrid4 As Label
+    Friend WithEvents lblGrid5 As Label
+    Friend WithEvents lblGrid6 As Label
+    Friend WithEvents lblGrid7 As Label
+    Friend WithEvents lblGrid8 As Label
+    Friend WithEvents lblGrid9 As Label
+    Friend WithEvents lblPlayer1GamesWon As Label
+    Friend WithEvents lblPlayer2Gameswon As Label
+    Friend WithEvents btnQuit As Button
+    Friend WithEvents btnNewGame As Button
 End Class
--- TicTacToe/Form1.vb
+++ TicTacToe/Form1.vb
@@ -1,3 +1,47 @@
 ﻿Public Class Form1
 
+    Private intPlayer1Wins As Integer
+    Private intPlayer2Wins As Integer
+
+    Private Sub Reset_Grid()
+        lblGrid1.Text = String.Empty
+        lblGrid2.Text = String.Empty
+        lblGrid3.Text = String.Empty
+        lblGrid4.Text = String.Empty
+        lblGrid5.Text = String.Empty
+        lblGrid6.Text = String.Empty
+        lblGrid7.Text = String.Empty
+        lblGrid8.Text = String.Empty
+        lblGrid9.Text = String.Empty
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        intPlayer1Wins = 0
+        intPlayer2Wins = 0
+        lblPlayer1GamesWon.Text = intPlayer1Wins.ToString
+        lblPlayer2Gameswon.Text = intPlayer2Wins.ToString
+
+    End Sub
+
+    Private Sub btnQuit_Click(sender As Object, e As EventArgs) Handles btnQuit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnNewGame_Click(sender As Object, e As EventArgs) Handles btnNewGame.Click
+        Dim strContentOfGrid As String
+        strContentOfGrid = lblGrid1.Text + lblGrid2.Text + lblGrid3.Text + lblGrid4.Text + lblGrid5.Text + lblGrid6.Text + lblGrid7.Text +
+            lblGrid8.Text + lblGrid9.Text
+        If strContentOfGrid <> String.Empty Then
+            Dim dlgAnswer As DialogResult
+            dlgAnswer = MessageBox.Show("New Game", "Starting a new game will reset both players wins total back to zero." + ControlChars.NewLine +
+                                        "Are you sure you want to start a new game?", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation)
+            If dlgAnswer = DialogResult.Yes Then
+                Reset_Grid()
+                intPlayer1Wins = 0
+                intPlayer2Wins = 0
+                lblPlayer1GamesWon.Text = intPlayer1Wins.ToString
+                lblPlayer2Gameswon.Text = intPlayer2Wins.ToString
+            End If
+        End If
+    End Sub
 End Class
